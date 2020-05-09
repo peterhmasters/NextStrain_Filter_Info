8d6d24ae7feb2fba315fe3ec04d937da5824436d
peterhmasters <peterhmasters@gmail.com>
2020-05-09 00:43:08 -0400

    Added docstrings

--- TrackFilter.py
+++ TrackFilter.py
@@ -35,31 +35,88 @@ import pandas as pd
 
 
 class TrackFilter:
+    """This class tracks which sequences are included or excluded by the filter and the reason why
+
+    :param seqs: A list of sequences to be filtered on
+    :type seqs: list
+
+    :Example:
+
+    >>> seqs = ['PRVABC59','COL/FLR_00008/2015','Colombia/2016/ZC204Se']
+    >>> test = TrackFilter(seqs)
+    >>> assert test.seqs == seqs
+    >>> assert test.df.shape == (len(seqs), 3)
+
+
+    """
     def __init__(self, seqs):
+        """Constructor method
+
+        :keyword df tracking sequence name, status to include or not, and the reason for either
+        """
         self.seqs = seqs
+        # Initializing each sequenced as included and passed all tests
         self.df = pd.DataFrame(
             list(zip(seqs, ["inclusion"] * len(seqs), ["passed"] * len(seqs))),
             columns=["Sequence", "Status", "Reason"],
         )
 
-    def update(self, type, reason, new_seqs):
+    def update(self, action, reason, new_seqs):
+        """This function updates the dataframe tracking sequences based on if they need to be excluded or included
+
+        :param action: If these are the sequences that to not be removed ('remove'), or the sequenced to be added ("add")
+        :type action: str
+        :param reason: The reason why are these sequences being added or removed
+        :type reason: str
+        :param new_seqs: The list of sequenced that need to not be removed or to be added
+        :type new_seqs: list
+        :return: None
+        :rtype: None
+
+        :Example:
+
+        >>> seqs = ['PRVABC59','COL/FLR_00008/2015','Colombia/2016/ZC204Se']
+        >>> test = TrackFilter(seqs)
+        >>> test.update('remove', 'too short', ['PRVABC59','COL/FLR_00008/2015'])
+        >>> assert test.seqs == ['PRVABC59','COL/FLR_00008/2015']
+
+
+        """
+        # Updating the list of sequences, the self.seqs update isn't used anywhere but is future-proofing
+        # In case this class could be used to track sequences instead of global variables
         self.seqs = (
             new_seqs
-            if type == "remove"
+            if action == "remove"
             else self.seqs + list(filter(lambda seq: seq not in self.seqs, new_seqs))
         )
+        # Updating the dataframe if the sequence needs to be removed or it needs to be added and the reason why
         self.df = pd.DataFrame(
             map(
                 lambda sequence: [
                     sequence[0],
-                    "exclusion" if type == "remove" else "inclusion",
+                    "exclusion" if action == "remove" else "inclusion",
                     reason
-                    if ((type == "remove") == (sequence[1] == "inclusion"))
+                    # only updating the Reason if the Status is changing
+                    if ((action == "remove") == (sequence[1] == "inclusion"))
                     else sequence[2],
                 ]
-                if ((type == "remove") != (sequence[0] in new_seqs))
+                # only updating the sequence if it's remove & not in the sequence or add & in the sequence
+                if ((action == "remove") != (sequence[0] in new_seqs))
                 else sequence,
                 self.df.itertuples(index=False),
             ),
             columns=["Sequence", "Status", "Reason"],
         )
+
+    def summary(self, text_output, csv_output):
+        """Conditionally printing out a text summary and a csv containing filter information
+
+        :param text_output: To print out the text report or not
+        :type text_output: Bool
+        :param csv_output: To output the csv report or not
+        :type csv_output: Bool
+        :return: None
+        :rtype: None
+        """
+        if csv_output: self.df.sort_values(["Status", "Reason"]).to_csv("Filter_Results.csv")
+        if text_output: print(self.df.groupby(["Status", "Reason"]).count())
--- test.py
+++ test.py
@@ -1,13 +1,12 @@
 from Bio import SeqIO
 from TrackFilter import TrackFilter
-import pandas as pd
-from functools import reduce
 
 seqs = list(SeqIO.to_dict(SeqIO.parse("sequences.fasta", "fasta")).keys())
 test = TrackFilter(seqs)
 assert test.seqs == seqs
 assert test.df.shape == (len(seqs), 3)
 test.update("remove", "too short", ["PAN/CDC_259359_V1_V3/2015","COL/FLR_00024/2015"])
+assert test.seqs == ["PAN/CDC_259359_V1_V3/2015","COL/FLR_00024/2015"]
 assert test.df.iloc[0,1] == "inclusion"
 assert test.df.iloc[0,2] == "passed"
 assert test.df.iloc[1,1] == "inclusion"
@@ -28,3 +27,4 @@ assert test.df.iloc[1,1] == "inclusion"
 assert test.df.iloc[1,2] == "changed my mind"
 assert test.df.iloc[2,1] == "exclusion"
 assert test.df.iloc[2,2] == "too short"
+test.summary(True, True)
